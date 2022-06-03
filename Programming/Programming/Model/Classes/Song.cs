340f325965067fb0e02a65cea3892faf392285cc
Максим <akaikinmax@mail.ru>
2022-06-03 20:59:09 +0700

    com

--- Programming/Programming/Model/Classes/Song.cs
+++ Programming/Programming/Model/Classes/Song.cs
@@ -6,14 +6,30 @@ using System.Threading.Tasks;
 
 namespace Programming.Model.Classes
 {
+    /// <summary>
+    /// Хранит данные о песне.
+    /// </summary>
     public class Song
     {
+        /// <summary>
+        /// Продолжительность песни в секундах.
+        /// </summary>
         private double _duration;
 
+        /// <summary>
+        /// Создаёт экземпляр класса <see cref="Song"/>.
+        /// </summary>
         public Song()
         {
         }
 
+        /// <summary>
+        /// Создаёт экземпляр класса <see cref="Song"/>.
+        /// </summary>
+        /// <param name="name">Название песни.</param>
+        /// <param name="duration">Продолжительность песни в секундах. Должна быть положительной.</param>
+        /// <param name="genre">Жанр песни.</param>
+        /// <param name="author">Автор песни.</param>
         public Song(string name,
                     double duration,
                     string genre,
@@ -24,12 +40,24 @@ namespace Programming.Model.Classes
 
         }
 
+        /// <summary>
+        /// Возвращает и задаёт имя музыканта.
+        /// </summary>
         public string Author { get; set; }
 
+        /// <summary>
+        /// Возвращает и задает жанр песни.
+        /// </summary>
         public string Genre { get; set; }
 
+        /// <summary>
+        /// Возвращает и задает название песни.
+        /// </summary>
         public string Name { get; set; }
 
+        /// <summary>
+        /// Возвращает и задаёт продолжительность песни в секундах. Должно быть положительным числом.
+        /// </summary>
         public double Duration
         {
             get
--- Programming/Programming/Model/Classes/flight.cs
+++ Programming/Programming/Model/Classes/flight.cs
@@ -6,14 +6,29 @@ using System.Threading.Tasks;
 
 namespace Programming.Model.Classes
 {
+    /// <summary>
+    /// Хранит данные о рейсе.
+    /// </summary>
     public class Flight
     {
+        /// <summary>
+        /// Время полёта в минутах.
+        /// </summary>
         private int _time;
 
+        /// <summary>
+        /// Создаёт экземпляр класса <see cref="Flight"/>.
+        /// </summary>
         public Flight()
         {
         }
 
+        /// <summary>
+        /// Создаёт экземпляр класса <see cref="Flight"/>.
+        /// </summary>
+        /// <param name="departure">Место отправления.</param>
+        /// <param name="destination">Место прибытия</param>
+        /// <param name="time">Время полёта в минутах. Должно быть положительным числом.</param>
         public Flight(string departure,
                     string destination,
                     int time)
@@ -23,10 +38,19 @@ namespace Programming.Model.Classes
             Time = time;
         }
 
+        /// <summary>
+        /// Возвращает и задаёт место отправления.
+        /// </summary>
         public string Departure { get; set; }
 
+        /// <summary>
+        /// Возвращает и задаёт место прибытия.
+        /// </summary>
         public string Destination { get; set; }
 
+        /// <summary>
+        /// Возвращает и задаёт время полёта в минутах. Должно быть положительным числом.
+        /// </summary>
         public int Time
         {
             get
